f5077e5058a0b4ecf0d518f94f18a4e5c5bee09b
cristiann <cristianandres1496@gmail.com>
2026-07-11 16:33:22 -0500

    input component

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import Input from '../components/Input';
 
 const Signup = () => {
   const [fullName, setFullName] = useState("");
@@ -13,6 +14,59 @@ const Signup = () => {
     <div className='h-screen w-full relative flex items-center justify-center overflow-hidden'>
       {/* Background image with blur*/}
       <img src={assets.login_bg} alt="Background" className='absolute insert-0 w-full h-full object-cover filter blur-sm' />
+      <div className='relative z-10 w-full max-w-lg px-6'>
+        <div className="bg-white bg-opacity-95 backdrop-blur-sm rounded-lg shadow-2xl p-8 max-h-[90vh] overflow-y-auto">
+          <h3 className='text-2xl font-semibold text-black text-center mb-2'>
+            Create An Account
+          </h3>
+          <p className='text-sm text-slate-700 text-center mb-8'>
+            Start tracking your spendings by joing with us.
+          </p>
+
+          <form className='space-y-4'>
+            <div className='flex justify-center mb-6'>
+              {/* Profile image */}
+            </div>
+            <div className='grid grid-cols-2 md:grid-cols-2 gap-4'>
+              <Input
+                value={fullName} 
+                onChange={(e) => setFullName(e.target.value)} 
+                label={"Full name"} 
+                placeholder={"Jhon Doe"} 
+                type={"text"}
+                />
+              <Input
+                value={email} 
+                onChange={(e) => setEmail(e.target.value)} 
+                label={"Email Address"} 
+                placeholder={"name@example.com"} 
+                type={"text"}
+                />
+              <div className='col-span-2'>
+                <Input
+                  value={password} 
+                  onChange={(e) => setPassword(e.target.value)} 
+                  label={"Password"} 
+                  placeholder={"**********"} 
+                  type={"password"}
+                  />
+              </div>
+            </div>
+            {error & (
+              <p className='text-red-800 text-sm text-center bg-red-50 p-2 rounded'>
+                {error}
+              </p>
+            )}
+            <button className='btn-primary w-full py-3 text-lg font-medium' type='submit'>
+              SIGN UP
+            </button>
+            <p className='text-sm text-slate-800 text-center mt-6'>
+              Already have an accout?
+              <Link to={"/login"} className='font-medium text-primary underline hover:text-primary-dark transition-colors'>Login</Link>
+            </p>
+          </form>
+        </div>
+      </div>
     </div>
   )
 }
